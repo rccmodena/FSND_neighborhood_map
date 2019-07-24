d5b2625a2a51cb6a143f48098970d6b93c095c98
Rudi César Comiotto Modena <rudi.modena@gmail.com>
2019-07-24 14:48:51 -0300

    feat: Add side menu with search bar

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+// Function to show the side menu
+function showSideMenu() {
+  document.getElementById("sideMenu").style.width = "400px";
+  document.getElementById("sideMenu").classList.toggle("container-fluid");
+}
+
+// Function to close the side menu
+function closeSideMenu() {
+  document.getElementById("sideMenu").style.width = "0";
+  document.getElementById("sideMenu").classList.toggle("container-fluid");
+}
+
+// Add the show side menu function to the hamburger button
+document.getElementById("hamburgerButton").addEventListener("click", showSideMenu);
+
+// Add the show side menu function to the hamburger button
+document.getElementById("closeButton").addEventListener("click", closeSideMenu);
