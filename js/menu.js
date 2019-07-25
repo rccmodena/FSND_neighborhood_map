ff093ef0d1a3a990fe2d0f0e6d2337bef4a162fb
Rudi César Comiotto Modena <rudi.modena@gmail.com>
2019-07-25 18:21:35 -0300

    docs: Add comments to the menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,22 @@
 // Function to show the side menu
 function showSideMenu() {
+  // Set the width of the side menu
   document.getElementById("sideMenu").style.width = "300px";
+  // Add the Bootstrap class container-fluid
   document.getElementById("sideMenu").classList.toggle("container-fluid");
+  // Set to visible the list of places
   document.getElementById("placesList").style.visibility = "visible";
-  document.getElementById("filterPlaces").focus(); 
+  // When open the side menu set the focus to the search bar
+  document.getElementById("filterPlaces").focus();
 }
 
 // Function to close the side menu
 function closeSideMenu() {
+  // Set the visibility of the list to hidden
   document.getElementById("placesList").style.visibility = "hidden";
+  //  Set the width of the side menu to 0
   document.getElementById("sideMenu").style.width = "0";
+  // Remove the Bootstrap class container-fluid
   document.getElementById("sideMenu").classList.toggle("container-fluid");
 }
 
